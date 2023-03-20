56cef72a24713939ba71934afcb8031fbccba735
HughMorris01 <gfarrell2@Live.com>
2023-03-20 11:54:51 -0400

    Add cube and controls, finish Section-1

--- index.js
+++ index.js
@@ -1,5 +1,59 @@
+//Cube
+let x = 0;
+let y = 20;
+let z = 0;
+let bool = true;
+let interval;
+
+const cube = document.querySelector('.cube')
+
+document.querySelector('.top-x-control').addEventListener('click', ()=>{
+    cube.style.transform = `rotateX(${x += 20}deg) rotateY(${y}deg) rotateZ(${z}deg)`
+})
+
+document.querySelector('.bottom-x-control').addEventListener('click', ()=>{
+    cube.style.transform = `rotateX(${x -= 20}deg) rotateY(${y}deg) rotateZ(${z}deg)`
+})
+
+document.querySelector('.left-y-control').addEventListener('click', ()=>{
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y -= 20}deg) rotateZ(${z}deg)`
+})
+document.querySelector('.right-y-control').addEventListener('click', ()=>{
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y += 20}deg) rotateZ(${z}deg)`
+})
+
+document.querySelector('.top-z-control').addEventListener('click', ()=>{
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${z -= 20}deg)`
+})
+document.querySelector('.bottom-z-control').addEventListener('click', ()=>{
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${z += 20}deg)`
+})
+
+const playPause = ()=>{
+    if(bool) {
+        interval = setInterval(()=>{
+            cube.style.transform = `rotateX(${x}deg) rotateY(${y++}deg) rotateZ(${z}deg)`
+        }, 100)
+    } else {
+        clearInterval(interval)
+    }
+}
+
+playPause()
+
+document.querySelector('.controls').addEventListener('mouseover', ()=> {
+    bool = false;
+    playPause()
+})
+
+document.querySelector('.controls').addEventListener('mouseout', ()=> {
+    bool = true;
+    playPause()
+})
+// End of Cube
+
 //Slideshow
-const slideshowDivs = () => {
+const slideshowDivs = ()=>{
     for(let i = 1; i <= 5; i++) {
         const div = document.createElement('div');
         div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.jpg)`;
@@ -14,9 +68,9 @@ slideshowDivs();
 
 const divs = document.querySelectorAll('.slideshow div')
 
-const slideshow = () => {
+const slideshow = ()=>{
     let a = 1;
-    setInterval(() => {
+    setInterval(()=>{
         a++
 
         const div = document.querySelector('.slideshow .change')
