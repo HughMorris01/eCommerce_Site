77288c55dc908bf1e910a9e35bdbacf7f6c1dbf0
HughMorris01 <gfarrell2@Live.com>
2023-03-19 23:08:25 -0400

    Initial Commit: logo, navbar and slideshow script

---
+++ index.js
@@ -0,0 +1,37 @@
+//Slideshow
+const slideshowDivs = () => {
+    for(let i = 1; i <= 5; i++) {
+        const div = document.createElement('div');
+        div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.jpg)`;
+
+        i === 1 && div.classList.add('change')
+
+        document.querySelector('.slideshow').appendChild(div);
+    }
+}
+
+slideshowDivs();
+
+const divs = document.querySelectorAll('.slideshow div')
+
+const slideshow = () => {
+    let a = 1;
+    setInterval(() => {
+        a++
+
+        const div = document.querySelector('.slideshow .change')
+
+        div.classList.remove('change')
+
+        if(a < divs.length) {
+            div.nextElementSibling.classList.add('change')
+        } else {
+            divs[0].classList.add('change');
+            a = 1;
+        }
+        
+    }, 8000)
+}
+
+slideshow();
+//End of Slideshow
